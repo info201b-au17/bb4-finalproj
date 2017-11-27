f2234c5060dc7cec3d13c4493d6ce2618f54cddf
mockingod <mockingod@gmail.com>
2017-11-27 14:35:02 -0800

    Separated loadData.R

---
+++ loadData.R
@@ -0,0 +1,30 @@
+# This file loads in data and writes the data to a local csv file so there's only one calculation
+
+updateData <- function() {
+  # Load in bike data
+  bikeTheftData <- read.csv("data/testDatasets/stolen_bikes.csv") %>% select(Latitude, Longitude, Year)
+  bikeRackData <- read.csv("data/testDatasets/bike_racks.csv") %>% select(LATITUDE, LONGITUDE, CONDITION)
+  
+  # Constants
+  radiusSensitivity <- 0.004 # Radius of bike thefts that will be considered for a bike rack, in units of lat/long
+  yearThreshold <- as.numeric(format(Sys.Date(),'%Y')) - 2 # Gets theft activity from the past two years
+  
+  # Add count of thefts near bike rack
+  # VERY SLOW PROCESS! Have to optimize or find a way around looping through entire dataset
+  # IDEA: Only grab theft data when CLICKING on the point on the map
+  # IDEA: Calculate nearby theft data as well to determine best spot to park
+  getTheftCount <- function(latIn, longIn) {
+    returnCount <- bikeTheftData %>% 
+      filter(abs(Latitude-(latIn)) < radiusSensitivity & 
+               abs(Longitude-longIn) < radiusSensitivity & 
+               Year >= yearThreshold) %>% 
+      nrow()
+    return(returnCount)
+  }
+  bikeRackData$THEFTCOUNT <- NA
+  for(i in 1:nrow(bikeRackData)) {
+    bikeRackData$THEFTCOUNT[i] <- getTheftCount(bikeRackData$LATITUDE[i], bikeRackData$LONGITUDE[i])
+  }
+  
+  write.csv(bikeRackData, file="data/testDatasets/bike_racks.csv")
+}
--- server.R
+++ server.R
@@ -13,44 +13,10 @@ library(ggplot2)
 # bikeTheftData <- bikeTheftDataset()
 # bikeRackData <- bikeRackDataset()
 
-# Get CSV data (Offline testing)
-bikeTheftData <- read.csv("data/testDatasets/stolen_bikes.csv") %>% select(Latitude, Longitude, Year)
-bikeRackData <- read.csv("data/testDatasets/bike_racks.csv") %>% select(LATITUDE, LONGITUDE, CONDITION)
+# Get data updating function
+source("loadData.R")
 
-# Constants
-radiusSensitivity <- 0.004 # Radius of bike thefts that will be considered for a bike rack, in units of lat/long
-yearThreshold <- as.numeric(format(Sys.Date(),'%Y')) - 2 # Gets theft activity from the past two years
-
-# Add count of thefts near bike rack
-# VERY SLOW PROCESS! Have to optimize or find a way around looping through entire dataset
-# IDEA: Only grab theft data when CLICKING on the point on the map
-# IDEA: Calculate nearby theft data as well to determine best spot to park
-
-getTheftCount <- function(latIn, longIn) {
-  returnCount <- bikeTheftData %>% 
-    filter(abs(Latitude-(latIn)) < radiusSensitivity & 
-             abs(Longitude-longIn) < radiusSensitivity & 
-             Year >= yearThreshold) %>% 
-    nrow()
-  return(returnCount)
-}
-bikeRackData$THEFTCOUNT <- NA
-for(i in 1:nrow(bikeRackData)) {
-  bikeRackData$THEFTCOUNT[i] <- getTheftCount(bikeRackData$LATITUDE[i], bikeRackData$LONGITUDE[i])
-}
-
-############
-leaflet() %>% addTiles() %>% addProviderTiles("Esri.WorldStreetMap") %>%
-  setView(lng = -122.3129, lat = 47.6563, zoom = 17) %>%
-  addCircles(lng = as.numeric(bikeRackData$LONGITUDE), 
-             lat = as.numeric(bikeRackData$LATITUDE), 
-             weight = 1, 
-             radius = 4, 
-             popup = paste(bikeRackData$THEFTCOUNT, " ", bikeRackData$LATITUDE, " ", bikeRackData$LONGITUDE), 
-             color = "#FFA500", 
-             fillOpacity = 1.0)
-#############
-
+bikeRackData <- read.csv("data/testDatasets/bike_racks.csv")
 
 shinyServer(function(input, output) {
   output$CountryMap <- renderLeaflet({
