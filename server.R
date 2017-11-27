379a55d1c8fb41eca31860365717855dd2f5df40
mockingod <mockingod@gmail.com>
2017-11-27 10:58:06 -0800

    Added theft count calculation. TODO: VERY SLOW Calculation, read IDEAs in server.R

--- server.R
+++ server.R
@@ -19,6 +19,22 @@ bikeRackData <- read.csv("data/testDatasets/bike_racks.csv") %>% select(LATITUDE
 
 # Constants
 radiusSensitivity <- 0.004 # Radius of bike thefts that will be considered for a bike rack, in units of lat/long
+yearThreshold <- as.numeric(format(Sys.Date(),'%Y')) - 2 # Gets theft activity from the past two years
+
+# Add count of thefts near bike rack
+# VERY SLOW PROCESS! Have to optimize or find a way around looping through entire dataset
+# IDEA: Only grab theft data when CLICKING on the point on the map
+# IDEA: Calculate nearby theft data as well to determine best spot to park
+getTheftCount <- function(latIn, longIn) {
+  returnCount <- bikeTheftData %>% 
+    filter(abs(Latitude-(latIn)) < 0.004 & abs(Longitude-longIn) < 0.004 & Year >= yearThreshold) %>% 
+    nrow()
+  return(returnCount)
+}
+bikeRackData$THEFTCOUNT <- NA
+for(i in 1:nrow(bikeRackData)) {
+  bikeRackData$THEFTCOUNT[i] <- getTheftCount(bikeRackData$LATITUDE[i], bikeRackData$LONGITUDE[i])
+}
 
 
 shinyServer(function(input, output) {
