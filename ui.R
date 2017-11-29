679457eaa7a4f763a9ef91cbc4a52c952d14735a
mockingod <mockingod@gmail.com>
2017-11-29 08:37:47 -0800

    Made map fit to window nicely

--- ui.R
+++ ui.R
@@ -7,6 +7,17 @@ library(shinyjs)
 
 shinyUI(fluidPage(
   useShinyjs(),
+  inlineCSS("#loading_page { 
+                  margin-left:50 wv; 
+                  text-align:center; 
+                  margin-top: 50vh; 
+                  padding:0px}
+            #main_content { 
+                  padding:0px;
+            }
+            .container-fluid {
+                  padding:0px;
+            }"),
   div(
     id = "loading_page",
     h1("Loading data, please wait...")
@@ -14,9 +25,7 @@ shinyUI(fluidPage(
   hidden(
     div(
       id = "main_content",
-      mainPanel(
-        leafletOutput("CountryMap", width = "90vw", height = "90vh")
-      )
+      leafletOutput("CountryMap", width = "100vw", height = "100vh")
     )
   )
    
